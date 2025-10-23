0495c1e3aea89622677a6d84db9203904097c98a
oozoofrog <oozoofrog@gmail.com>
2025-10-23 17:05:56 +0900

    Rename phase include files to numbered naming

--- lib/SIL/Parser/ParseSILInstructionVisitor.cpp
+++ lib/SIL/Parser/ParseSILInstructionVisitor.cpp
@@ -111,6 +111,6 @@ SILInstructionParserVisitor::dispatch(SILInstructionKind Opcode) {
 #include "ParseSILInstructionVisitorImpl/Phase3_11_Methods.inc"
 #include "ParseSILInstructionVisitorImpl/Phase3_11_Memory.inc"
 #include "ParseSILInstructionVisitorImpl/Phase3_12_Dynamic.inc"
-#include "ParseSILInstructionVisitorImpl/Phase3_N_Literal.inc"
-#include "ParseSILInstructionVisitorImpl/Phase3_N_OpenPack.inc"
+#include "ParseSILInstructionVisitorImpl/Phase3_14_Literal.inc"
+#include "ParseSILInstructionVisitorImpl/Phase3_15_OpenPack.inc"
 #include "ParseSILInstructionVisitorImpl/Phase3_13_Debug.inc"
